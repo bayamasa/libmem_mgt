406a2dc34682d58f12a56fa06d59b4efbed7471d
kiri0314 <t.kirihara0314@gmail.com>
2021-12-24 05:28:42 +0900

    [upgreade]合計サイズと確保数を表示する機能を追加

--- mem_mgt.c
+++ mem_mgt.c
@@ -81,6 +81,10 @@ void	mem_mgt_finish_check(int n)
 	size_t	i;
 	bool	first_flag;
 
+	printf("\x1b[33m-----プログラム終了時のヒープ領域-----\x1b[39m\n");
+	printf(" 合計サイズ : %zuバイト 確保数 : %zu個\n", \
+	g_mem_mgt.use_byte, g_mem_mgt.use_mem_info);
+	printf("\x1b[33m--------------------------------------\x1b[39m\n");
 	first_flag = true;
 	i = 0;
 	while (i < MAX_NUM)
@@ -106,23 +110,21 @@ void	mem_mgt_finish_check(int n)
 void	mem_mgt_check(const char *file, unsigned int line, const char *func)
 {
 	size_t	i;
-	bool	first_flag;
 
-	first_flag = true;
+	printf("\x1b[33m--%s:%s関数:%u行目のヒープ領域--\x1b[39m\n", file, func, line);
+	printf(" 合計サイズ : %zuバイト 確保数 : %zu個\n", \
+	g_mem_mgt.use_byte, g_mem_mgt.use_mem_info);
+	printf("\x1b[33m--------------------------------------\x1b[39m\n");
 	i = 0;
 	while (i < MAX_NUM)
 	{
 		if (g_mem_mgt.mem_info[i].ptr != NULL)
 		{
-			if (first_flag)
-				printf("\x1b[33m--%s:%s関数:%u行目のヒープ領域--\x1b[39m\n", \
-				file, func, line);
 			printf(" アドレス : %p\n", g_mem_mgt.mem_info[i].ptr);
 			printf(" サイズ   : %zuバイト\n", g_mem_mgt.mem_info[i].size);
 			printf(" 場所     : %s:%s関数:%u行目\n", g_mem_mgt.mem_info[i].file, \
 			g_mem_mgt.mem_info[i].func, g_mem_mgt.mem_info[i].line);
 			printf("\x1b[33m--------------------------------------\x1b[39m\n");
-			first_flag = false;
 		}
 		i++;
 	}
