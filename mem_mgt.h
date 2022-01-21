3af20ee153656a79f8364ee6fbe7e4eeefdd8b08
kiri0314 <t.kirihara0314@gmail.com>
2022-01-22 00:54:37 +0900

    clean

--- mem_mgt.h
+++ mem_mgt.h
@@ -29,6 +29,7 @@ typedef struct s_mem_mgt
 }	t_mem_mgt;
 
 /* 関数のプロトタイプ宣言 */
+
 void	*mem_mgt_malloc(size_t size, const char *file, unsigned int line, const char *func);
 void	mem_mgt_free(void *ptr);
 void	mem_mgt_finish_check(int n);
